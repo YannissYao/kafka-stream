8189f29d1e02f1fa35e5e389d7f548c52c9db23c
Yannis <834395851@qq.com>
2021-12-28 12:31:45 +0800

    fix

--- src/main/java/world/oasis/stream/map/CatCountMapFun.java
+++ src/main/java/world/oasis/stream/map/CatCountMapFun.java
@@ -4,6 +4,8 @@ import com.vip.vjtools.vjkit.mapper.JsonMapper;
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 
+import java.util.Objects;
+
 
 public class CatCountMapFun extends RichMapFunction<String, Tuple2<Integer, Integer>> {
 
@@ -11,6 +13,9 @@ public class CatCountMapFun extends RichMapFunction<String, Tuple2<Integer, Inte
     @Override
     public Tuple2<Integer, Integer> map(String s) {
         Tuple2<Integer, Integer> t2 = JsonMapper.INSTANCE.fromJson(s, Tuple2.class);
+        if (Objects.isNull(t2.f0)) {
+            t2.f0 = 0;
+        }
         return t2;
     }
 }
